5f83f6152e11596a971005e59bc6766f5ecbb2b3
thesimpleGithub <thesimple369@gmail.com>
2023-03-02 04:09:52 +0900

    React Next Ts Deploy

---
+++ _next/static/jukH9gNs3-Lt9KwnJ_nrW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8aa2d2e56444bbce.css","static/chunks/525-bb89e48f3ab7edd0.js","static/chunks/pages/index-266b92aee2021831.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
